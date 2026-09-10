6f2d7f8951e7a98c76c786b978cbb1c44a87899e
janechan735-lang <janechan735@gmail.com>
2026-09-10 11:05:52 +0800

    发布Banner与金额修复

--- sw.js
+++ sw.js
@@ -1,4 +1,12 @@
-const CACHE='chibao-v1'; const CORE=['/','/manifest.webmanifest','/wing.svg'];
+const CACHE='chibao-v2'
+const ROOT=new URL('./',self.registration.scope).toString()
+const CORE=[ROOT,new URL('./manifest.webmanifest',ROOT).toString(),new URL('./wing.svg',ROOT).toString()]
+
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE))))
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))))
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('/'))))})
+self.addEventListener('fetch',e=>{
+  if(e.request.method==='GET') e.respondWith(
+    fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r})
+      .catch(()=>caches.match(e.request).then(r=>r||caches.match(ROOT)))
+  )
+})
